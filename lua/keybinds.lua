8546b01019cdd885208a4ddf9b0671974d310681
lemont <itsrvn.dev@gmail.com>
2023-08-06 13:30:08 +0200

    Add move line mapping to visual mode

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -12,19 +12,20 @@ local mappings = {
         { '<C-h>',      ':nohlsearch<CR>' },
         { '<leader>bd', ':bd <bar> bp<CR>' },
         { '<leader>y',  '"+y' }, -- Yank to System Clipboard
-
         { '<leader>w',  ':w<CR>' },
         { '<C-n>',      ':NvimTreeToggle<CR>' },
         -- Telescope Mappings --
         { '<C-p>',      telescope.find_files },
         { '<C-b>',      telescope.buffers },
-        { '<M-j>',      ':move +1<CR>' },
-        { '<M-k>',      ':move -2<CR>' }
+        { '<A-j>',      ':move .+1<CR>==' },
+        { '<A-k>',      ':move .-2<CR>==' }
     },
     v = {
         { 'H',         '^' },
         { 'L',         '$' },
-        { '<leader>y', '"+y' }
+        { '<leader>y', '"+y' },
+        { '<A-j>',     ':move \'>+1<CR>gv=gv' },
+        { '<A-k>',     ':move \'<-2<CR>gv=gv' }
     },
 }
 
