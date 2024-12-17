54ec8db059daef046afca5f7c513efe54b419a65
lenosan <itsrvn.dev@gmail.com>
2024-12-17 06:41:29 +0100

    Add augroups to term

--- lua/term.lua
+++ lua/term.lua
@@ -9,6 +9,7 @@ local term_win = nil
 local function openTerm()
   if vim.fn.bufexists(term_buf) ~= 1 then
     aucmd('TermOpen', {
+      group = augrp('custom-term-open', { clear = true }),
       callback = function()
         vim.opt_local.number = false
         vim.opt_local.relativenumber = false
@@ -20,18 +21,19 @@ local function openTerm()
       callback = function()
         for _, bufs in ipairs(vim.fn.getbufinfo(term_buf)) do
           aucmd({ 'BufWriteCmd', 'FileWriteCmd', 'FileAppendCmd' }, {
+            group = augrp('custom-term-close', { clear = true }),
             buffer = bufs.bufnr,
             command = 'q!',
           })
         end
       end,
     })
-    vim.cmd([[split | wincmd J | resize 10 | term]])
+    vim.cmd([[split | wincmd J | resize 15 | term]])
     term_win = vim.fn.win_getid()
     term_buf = vim.fn.bufnr('%')
     vim.api.nvim_buf_set_name(term_buf, 'TERMINAL')
   elseif vim.fn.win_gotoid(term_win) ~= 1 then
-    vim.cmd('sbuffer ' .. term_buf .. '| wincmd J | resize 10')
+    vim.cmd('sbuffer ' .. term_buf .. '| wincmd J | resize 15')
     term_win = vim.fn.win_getid()
   end
   vim.cmd([[startinsert]])
