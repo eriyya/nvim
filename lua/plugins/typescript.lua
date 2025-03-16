4cfa5245b91a0a050e65e124c45f7f53bea07a04
lenosan <itsrvn.dev@gmail.com>
2025-03-16 06:15:50 +0100

    Remove `typescript-tools` for now

--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    'pmizio/typescript-tools.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-    config = function()
-      require('typescript-tools').setup({
-        settings = {
-          tsserver_plugins = {
-            '@styled/typescript-styled-plugin',
-          },
-        },
-      })
-    end,
-  },
+  -- {
+  --   'pmizio/typescript-tools.nvim',
+  --   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  --   config = function()
+  --     require('typescript-tools').setup({
+  --       settings = {
+  --         tsserver_plugins = {
+  --           '@styled/typescript-styled-plugin',
+  --         },
+  --       },
+  --     })
+  --   end,
+  -- },
 }
