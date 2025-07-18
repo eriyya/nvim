3dcb9414f9d7050b9705558ab49cf0a5d4e038de
onream <itsrvn.dev@gmail.com>
2025-07-18 06:52:33 +0200

    Remove other ai plugins, keep supermaven

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,35 +1,10 @@
 return {
-  -- Cursor like AI integration 
-  -- {
-  --   ' yetone/avante.nvim',
-  -- },
-  {
-    'supermaven-inc/supermaven-nvim',
-    config = function()
-      if not vim.settings.code_llm then
-        return
-      end
-      require('supermaven-nvim').setup({})
-      -- vim.api.nvim_set_hl(0, 'CmpItemKindSupermaven', { fg = '#6CC644' })
-    end,
-  },
-  {
-    'zbirenbaum/copilot.lua',
-    cmd = 'Copilot',
-    event = 'InsertEnter',
-    config = function()
-      if not vim.settings.code_llm then
-        return
-      end
-      require('copilot').setup({
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-          keymap = {
-            dismiss = '<C-l>',
-          },
-        },
-      })
-    end,
-  },
+  'supermaven-inc/supermaven-nvim',
+  config = function()
+    if not vim.settings.code_llm then
+      return
+    end
+    require('supermaven-nvim').setup({})
+    -- vim.api.nvim_set_hl(0, 'CmpItemKindSupermaven', { fg = '#6CC644' })
+  end,
 }
