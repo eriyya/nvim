8a8f6315ac2930a4e4f9ce8d211e92526f8ec2cf
onream <itsrvn.dev@gmail.com>
2025-07-22 00:44:00 +0200

    Formatting

--- lua/plugins/zen.lua
+++ lua/plugins/zen.lua
@@ -3,12 +3,12 @@ return {
   opts = {
     window = {
       backdrop = 1.0,
-      width = .65,
+      width = 0.65,
       options = {
         number = true,
         relativenumber = true,
         cursorline = true,
-      } 
-    }
+      },
+    },
   },
 }
