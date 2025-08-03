b7970006ec5f3e5715e1ab6965be092ba944e186
eriyya <itsrvn.dev@gmail.com>
2025-08-03 09:16:50 +0200

    Remove deprecated call to sign_define

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -41,11 +41,6 @@ local virtual_icons = {
   [vim.diagnostic.severity.HINT] = signs.Hint,
 }
 
-for type, icon in pairs(signs) do
-  local hl = 'DiagnosticSign' .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
 vim.diagnostic.config({
   virtual_text = {
     prefix = '',
